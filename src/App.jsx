7ad73b12c71db6921ac7982ebfcf00cd4248c591
Luis David <cusiluis04@gmail.com>
2024-01-22 17:35:40 -0500

    Juego al 80%

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,30 @@ const TURNS = {
   O: 'o'
 }
 
-const Square = ({children, updateBoard, index}) =>{
+const Square = ({children, isSelected, updateBoard, index}) =>{
+
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+  const handleClick  = () => {
+    updateBoard(index)
+  }
   return (
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
+
+const WINNER_COMBOS = [
+  [0, 1 ,2 ],
+  [3, 4 ,5 ],
+  [6, 7 ,8 ],
+  [0, 3 ,6 ],
+  [1, 4 ,7 ],
+  [2, 5 ,8 ],
+  [0, 4 ,8 ],
+  [2, 4 ,6 ]
+]
+
 
 function App() {
   // Creamos un array con 9 elementos
@@ -20,21 +37,71 @@ function App() {
     Array(9).fill(null)
   )
 
-  //Creamos un estado para saber de quien es el turno
+  //Creamos un estado para iniciar el turno en X
   const [turn, setTurn] = useState(TURNS.X)
+  // Creamos un estado para el ganador 
+  // null => No hay ganador
+  // false => Empate
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    // Revisamos las combinaciones ganadoras
+    // para verificar si X o O ganó
+    for(const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c] 
+      ){
+        return boardToCheck[a]
+      }
+    }
+    return null
+  }
+
+  const resetGame = () =>{
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+
+  }
+
+
+  const updateBoard = (index) => {
+    // no actualizacion la posición
+    // Si ya tiene algo 
+    if(board[index] || winner) return
+
+    // Actualizamos el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn    
+    setBoard(newBoard)
+
+    // Cambiamos el turno de jugador
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    // Revisamos si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+    setWinner(newWinner)
+    }
+  }
 
   return (
     <main className="board">
       <h1>TRES EN RAYA</h1>
       <section className="game">
         {
-          board.map((index) =>{
+          board.map((_ ,index) =>{
             return(
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
-
+                {board[index]}
               </Square>
             )
           })
@@ -49,6 +116,31 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false
+                  ? 'Empate'
+                  : 'Ganó'
+                }
+              </h2>
+
+              <header className="win">                
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
+
+          </section>
+        )
+      }
     </main>
   )
 }
